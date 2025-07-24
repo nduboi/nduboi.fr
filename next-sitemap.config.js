dcceaf46b3a11cab724a0f41360eefe3db481ee4
Nduboi <63715705+nduboi@users.noreply.github.com>
2025-07-24 13:29:20 +0200

    feat: Add next-sitemap for automated sitemap generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://nduboi.fr',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: 'monthly',
+  priority: 0.7,
+};
